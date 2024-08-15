a857b3efaab49b607c315008703f20b65921cc5c
Penny Slung <walter4Coece@gmail.com>
2024-08-15 16:26:49 +0300

    ce se intampla

---
+++ src/pages/Drawer.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer
+          class="bg-purple"
+          theme="dark"
+          permanent
+          width="500"
+        >
+          <v-list color="transparent">
+            <v-list-item title="PISISICA" class="pa-10"></v-list-item>
+            <v-list-item prepend-icon="mdi-folder-outline" title="ADD STUFF" class="pa-5"></v-list-item>
+            <v-list-item prepend-icon="mdi-folder-outline" title="placeholder" class="pa-5"></v-list-item>
+          </v-list>
+
+          <v-container style="align-items: center; margin-left: 35px;">
+            <List style="max-height: 300px;" />
+          </v-container>
+          <template v-slot:append>
+            <div class="pa-6">
+              <v-btn block>
+                Add Note
+              </v-btn>
+            </div>
+          </template>
+        </v-navigation-drawer>
+        <v-main style="height: 100vh; width: 50vw;" />
+      </v-layout>
+    </v-card>
+  </template>
+
+  <script lang="ts">
+    import { defineComponent} from 'vue';
+    import List from '@/components/List.vue';
+    
+    export default defineComponent({
+      data ()
+      {
+
+        return {
+            
+        }
+      }
+     });
+  </script>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/Drawer': RouteRecordInfo<'/Drawer', '/Drawer', Record<never, never>, Record<never, never>>,
     '/testlist': RouteRecordInfo<'/testlist', '/testlist', Record<never, never>, Record<never, never>>,
   }
 }
