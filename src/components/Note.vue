b9affad9e2e87259a989b0a114af0a35694e9b33
Penny Slung <walter4Coece@gmail.com>
2024-08-15 17:47:46 +0300

    e gata

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,6 +1,6 @@
 <template>
-		<v-card style="max-width: 600px; display: block; margin: auto;">
-			<v-card-title class="text-h5 grey lighten-2">Add New Task</v-card-title>
+		<v-card style="max-width: 600px; display: block; margin: auto; background-color: purple; border-radius: 70px;">
+			<v-card-title class="text-h5 lighten-2" style="text-align: center; color: black; text-shadow: #c034fb 2px 1px;">Add New Task</v-card-title>
 			<v-form class="mx-4 mt-4 pb-4" ref="form" style="margin-left: auto;">
 			  <v-text-field
 				v-model="newTask.title"
@@ -9,10 +9,10 @@
 				outlined
 			  ></v-text-field>
 			  <v-textarea label="Note" v-model="newTask.note" outlined></v-textarea>
-			  <div class="d-flex justify-end">
-				<v-btn plain class="mr-2" @click="cancelButtonClick">Cancel</v-btn>
-				<v-btn color="primary">Add</v-btn>
-			  </div>
+			  <div class="d-flex justify-center">
+					<v-btn plain class="mr-2" @click="cancelButtonClick">Cancel</v-btn>
+					<v-btn color="white">Add</v-btn>
+			</div>
 			</v-form>
 		</v-card>
 	
