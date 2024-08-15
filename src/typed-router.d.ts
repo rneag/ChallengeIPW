2de55b696e5132f572a6b4b2c8587df7e78916ef
Rares-Ioan Neagu <raresioanneagu@gmail.com>
2024-08-15 16:56:55 +0300

    pula de neho

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,6 +19,5 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/Button': RouteRecordInfo<'/Button', '/Button', Record<never, never>, Record<never, never>>,
   }
 }
